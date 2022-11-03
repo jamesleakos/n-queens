d3547555a14635e12be2b71fb8e542df7054b3dc
James Leakos <jamesleakos@gmail.com>
2022-11-03 15:03:30 -0700

    cleaned up some awkward callbacks after watching the solution video

--- src/solvers.js
+++ src/solvers.js
@@ -1,3 +1,4 @@
+
 /*           _
    ___  ___ | |_   _____ _ __ ___
   / __|/ _ \| \ \ / / _ \ '__/ __|
@@ -17,23 +18,33 @@
 
 window.findNRooksSolution = function(n) {
   var testBoard = new Board({n: n});
-  var solutions = advanced(0, n, testBoard, testBoard.hasAnyRookConflictsOn.bind(testBoard), pusher, []);
-  return solutions[0];
+  var solution = solver(0, n, testBoard, testBoard.hasAnyRookConflictsOn.bind(testBoard), function () {
+    return testBoard.rows().map(function(arr) {
+      return arr.slice();
+    });
+  });
+  return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
   var testBoard = new Board({n: n});
-  // var solutions = returnAllSolutions(0, n, testBoard, testBoard.hasAnyRookConflictsOn.bind(testBoard));
-  var solution = advanced(0, n, testBoard, testBoard.hasAnyRookConflictsOn.bind(testBoard), counter, [0]);
-  return solution[0];
+  var count = 0;
+  var solution = solver(0, n, testBoard, testBoard.hasAnyRookConflictsOn.bind(testBoard), function () {
+    count++;
+  });
+  return count;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  if (n === 0) { return []; }
   var testBoard = new Board({n: n});
-  var solutions = advanced(0, n, testBoard, testBoard.hasAnyQueenConflictsOn.bind(testBoard), pusher, []);
+  var solutions = [];
+  solver(0, n, testBoard, testBoard.hasAnyQueenConflictsOn.bind(testBoard), function() {
+    solutions.push(testBoard.rows().map(function(arr) {
+      return arr.slice();
+    }));
+  });
   if (solutions.length === 0) { return new Board({n: n}).rows(); }
   return solutions[0];
 };
@@ -41,67 +52,29 @@ window.findNQueensSolution = function(n) {
 // second for testing
 window.countNQueensSolutions = function(n) {
   if (n === 0) { return 1; }
+  var count = 0;
   var testBoard = new Board({n: n});
-  // var solutions = returnAllSolutions(0, n, testBoard, testBoard.hasAnyQueenConflictsOn.bind(testBoard));
-  var solution = advanced(0, n, testBoard, testBoard.hasAnyQueenConflictsOn.bind(testBoard), counter, [0]);
-  return solution[0];
-};
-
-var returnAllSolutions = function (rowIndex, n, board, spaceChecker, sols = []) {
-  for (var i = 0; i < n; i++) {
-    board.togglePiece(rowIndex, i);
-    if (!spaceChecker(rowIndex, i)) {
-      if (rowIndex === n - 1) {
-        sols.push(board.rows().map(function(arr) {
-          return arr.slice();
-        }));
-      } else {
-        returnAllSolutions(rowIndex + 1, n, board, spaceChecker, sols);
-      }
-    }
-    board.togglePiece(rowIndex, i);
-  }
-  return sols;
+  solver(0, n, testBoard, testBoard.hasAnyQueenConflictsOn.bind(testBoard), function () {
+    count++;
+  });
+  return count;
 };
 
 // give a another callback function to see if I can affect what happens when a solution is found
-var advanced = function (rowIndex, n1, board, spaceChecker, findCB, acc) {
+var solver = function (rowIndex, n1, board, spaceChecker, foundSolCallback) {
   for (var i = 0; i < n1; i++) {
     board.togglePiece(rowIndex, i);
     if (!spaceChecker(rowIndex, i)) {
       if (rowIndex === n1 - 1) {
-        let r = findCB(board, acc);
-        // if (r) {
-        //   return r;
-        // }
+        let r = foundSolCallback();
+        if (r) { return r; }
       } else {
-        // let r = advanced(rowIndex + 1, n1, board, spaceChecker, findCB, acc);
-        // if (r !== undefined) {
-        //   return r;
-        // }
-        advanced(rowIndex + 1, n1, board, spaceChecker, findCB, acc);
+        let r = solver(rowIndex + 1, n1, board, spaceChecker, foundSolCallback);
+        if (r) { return r; }
       }
     }
     board.togglePiece(rowIndex, i);
   }
-  return acc;
-};
-
-// var returnPusher = function (board, acc) {
-//   acc.push(board.rows().map(function(arr) {
-//     return arr.slice();
-//   }));
-//   return acc;
-// };
-
-var pusher = function (board, acc) {
-  acc.push(board.rows().map(function(arr) {
-    return arr.slice();
-  }));
-};
-
-var counter = function (board, acc) {
-  acc[0]++;
 };
 
 
